5fd1f8db3afd1989a0c8ea0137a04a2feacbd037
meg-engineer <cyobrin@gmail.com>
2020-07-31 17:09:56 +0900

    Headerレスポンシブ対応

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,36 +1,81 @@
 <template>
-  <div class="navigation-bar">
-    <v-btn text x-large style="text-transform: none" @click="$vuetify.goTo(0)"
-      >Home</v-btn
-    >
-    <v-btn
-      text
-      x-large
-      style="text-transform: none"
-      @click="$vuetify.goTo('#about', options)"
-      >About</v-btn
-    >
-    <v-btn
-      text
-      x-large
-      style="text-transform: none"
-      @click="$vuetify.goTo('#portfolio', options)"
-      >Portfolio</v-btn
-    >
-    <v-btn
-      text
-      x-large
-      style="text-transform: none"
-      @click="$vuetify.goTo('#skill', options)"
-      >Skill</v-btn
-    >
-    <v-btn
-      text
-      x-large
-      style="text-transform: none"
-      @click="$vuetify.goTo('#contact', options)"
-      >Contact</v-btn
-    >
+  <div>
+    <v-row>
+      <v-col cols="12" class="pa-0">
+        <v-toolbar color="rgba(250, 250, 250)" class="px-10 elevation-0">
+          <v-row align="center" justify="center" color="rgba(250, 250, 250)">
+            <v-toolbar-items class="hidden-sm-and-down">
+              <v-btn
+                text
+                x-large
+                style="text-transform: none"
+                @click="$vuetify.goTo(0)"
+                >Home</v-btn
+              >
+              <v-btn
+                text
+                x-large
+                style="text-transform: none"
+                @click="$vuetify.goTo('#about', options)"
+                >About</v-btn
+              >
+              <v-btn
+                text
+                x-large
+                style="text-transform: none"
+                @click="$vuetify.goTo('#portfolio', options)"
+                >Portfolio</v-btn
+              >
+              <v-btn
+                text
+                x-large
+                style="text-transform: none"
+                @click="$vuetify.goTo('#skill', options)"
+                >Skill</v-btn
+              >
+              <v-btn
+                text
+                x-large
+                style="text-transform: none"
+                @click="$vuetify.goTo('#contact', options)"
+                >Contact</v-btn
+              >
+            </v-toolbar-items>
+            <!-- ここからレスポンシブ対応 -->
+            <v-toolbar-icon class="hidden-md-and-up">
+              <v-btn
+                text
+                small
+                style="text-transform: none"
+                @click="$vuetify.goTo('#about', options)"
+                >About</v-btn
+              >
+              <v-btn
+                text
+                small
+                style="text-transform: none"
+                @click="$vuetify.goTo('#portfolio', options)"
+                >Portfolio</v-btn
+              >
+              <v-btn
+                text
+                small
+                style="text-transform: none"
+                @click="$vuetify.goTo('#skill', options)"
+                >Skill</v-btn
+              >
+              <v-btn
+                text
+                small
+                style="text-transform: none"
+                @click="$vuetify.goTo('#contact', options)"
+                >Contact</v-btn
+              >
+            </v-toolbar-icon>
+          </v-row>
+        </v-toolbar>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -39,21 +84,3 @@ export default {
   name: "AppNavigation"
 };
 </script>
-
-<style scoped>
-.navigation-bar {
-  background: rgba(250, 250, 250);
-  text-align: center;
-  padding: 10px;
-}
-v-btn {
-  color: black;
-  text-decoration: none;
-  margin: 50px;
-  text-transform: none;
-}
-
-v-btn:hover {
-  color: pink;
-}
-</style>
